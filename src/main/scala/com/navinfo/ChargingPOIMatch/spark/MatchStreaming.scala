80e3d429e9b178410ee9af7af99e598fe4253128
fangshaowei <fangshaowei@navinfo.com>
2016-01-26 19:32:08 +0800

    优化结构

--- src/main/scala/com/navinfo/ChargingPOIMatch/spark/MatchStreaming.scala
+++ src/main/scala/com/navinfo/ChargingPOIMatch/spark/MatchStreaming.scala
@@ -1,7 +1,5 @@
 package com.navinfo.ChargingPOIMatch.spark
 
-import java.text.SimpleDateFormat
-
 import com.alibaba.fastjson.JSON
 import com.mongodb.BasicDBObject
 import com.navinfo.ChargingPOIMatch.mongodb.MongoDBUtil
@@ -34,35 +32,20 @@ class MatchStreaming {
     println("==================== match start !!! ====================")
     println("======================= fetch lines count =========================")
     lines.count().foreachRDD(_.foreach(println))
-
     matchInfo(lines)
-
     ssc.start
     ssc.awaitTermination()
-
   }
 
   def matchInfo(lines: DStream[String]): Unit = {
     lines.foreachRDD(rdd => {
       rdd.map(JSON.parseObject).foreach(json => {
 
-        val sdf = new SimpleDateFormat("yyyyMMddhh:mm") // 线程不安全
-        val dateSdf = new SimpleDateFormat("yyyyMMdd")
+//        val sdf = new SimpleDateFormat("yyyyMMddhh:mm") // 线程不安全
+//        val dateSdf = new SimpleDateFormat("yyyyMMdd")
 
         val price = json.getString("price")
-        val staOpstate = json.getString("staOpstate")
-        var staOpstateInt = -1
-        if (staOpstate == null || staOpstate.length() == 0) {
-          staOpstateInt = -1
-        } else if (staOpstate.equals("初始") || staOpstate.equals("暂停营业")) {
-          staOpstateInt = 1
-        } else if (staOpstate.equals("运营中")) {
-          staOpstateInt = 0
-        } else if (staOpstate.equals("待运营")) {
-          staOpstateInt = 3
-        } else if (staOpstate.equals("关闭")) {
-          staOpstateInt = 5
-        }
+        val staOpstateInt = staOpstateChange(json.getString("staOpstate"))
 
         val acableNum = json.getInteger("acableNum")
         val dcableNum = json.getInteger("dcableNum")
@@ -72,61 +55,53 @@ class MatchStreaming {
         val priceList = json.getJSONArray("priceList")
         var chargeFee = "-1"
         var serveFee = "-1"
-        var timeRanges = new Array[String](2)
-        val currentTime = System.currentTimeMillis()
+        var timeRange = ""
+//        var timeRanges = new Array[String](2)
+//        val currentTime = System.currentTimeMillis()
         for (i <- 0 until priceList.size()) {
           val priceJson = JSON.parseObject(String.valueOf(priceList.get(i)))
-          timeRanges = priceJson.getString("timeRange").split("~")
-          val minTime = sdf.parse(dateSdf.format(currentTime) + timeRanges(0)).getTime
-          val maxTime = sdf.parse(dateSdf.format(currentTime) + timeRanges(1)).getTime
-          if (currentTime > minTime && currentTime <= maxTime) {
-            chargeFee = priceJson.getString("electricPrice")
-            if (chargeFee == null || chargeFee.length() == 0) chargeFee = "-1"
-            serveFee = priceJson.getString("servicePrice")
-            if (serveFee == null || serveFee.length() == 0) serveFee = "-1"
-          }
+          timeRange += priceJson.getString("timeRange") + ","
+          //          timeRanges = priceJson.getString("timeRange").split("~")
+          //          val minTime = sdf.parse(dateSdf.format(currentTime) + timeRanges(0)).getTime
+          //          val maxTime = sdf.parse(dateSdf.format(currentTime) + timeRanges(1)).getTime
+          //          if (currentTime > minTime && currentTime <= maxTime) {
+          chargeFee += priceJson.getString("electricPrice") + ","
+          if (chargeFee == null || chargeFee.length() == 0) chargeFee = "-1"
+          serveFee += priceJson.getString("servicePrice") + ","
+          if (serveFee == null || serveFee.length() == 0) serveFee = "-1"
+          //          }
         }
-
         val pileList = json.getJSONArray("pileList")
         for (i <- 0 until pileList.size()) {
           val pileJson = JSON.parseObject(String.valueOf(pileList.get(i)))
           val pileCode = pileJson.getString("pileCode")
-          val pileState = pileJson.getString("pileState")
 
-          var state = -1
-          if (pileState == null || pileState.length() == 0) {
-            state = -1
-          } else if (pileState.equals("空闲")) {
-            state = 0
-          } else if (pileState.equals("已插枪") || pileState.equals("已充满") || pileState.equals("涓流充") || pileState.equals("充电中") || pileState.equals("暂停")) {
-            state = 1
-          } else if (pileState.equals("离网") || pileState.equals("故障")) {
-            state = 2
-          }
+          val state = pileStateChange(pileJson.getString("pileState"))
 
           val query = new BasicDBObject("sockerParams.factory_num", pileCode) //1101150019101
           val updateItem = new BasicDBObject
+          updateItem.put("sockerParams.$.charge_fee", chargeFee.substring(0, chargeFee.length - 1))
+          updateItem.put("sockerParams.$.serve_fee", serveFee.substring(0, serveFee.length - 1))
+          updateItem.put("sockerParams.$.timeRange", timeRange.substring(0, timeRange.length - 1))
           updateItem.put("sockerParams.$.sockerState", new Integer(state))
-          updateItem.put("sockerParams.$.charge_fee", chargeFee)
-          updateItem.put("sockerParams.$.serve_fee", serveFee)
-          //          if(i == 0){
           updateItem.put("socker_num.sockableall_num", new Integer(ableNum))
           updateItem.put("price", price)
           updateItem.put("state", new Integer(staOpstateInt))
           updateItem.put("socker_num.dcableNum", dcableNum)
           updateItem.put("socker_num.acableNum", acableNum)
-          //          }
+
           val result = MongoDBUtil.update(query, updateItem, MatchStreaming.table)
 
           if (result.getModifiedCount > 0) {
             println("==================" + pileCode)
-            println("==================state: " + state + "==============pileState: " + pileState)
-            println("==================timeRanges: " + timeRanges(0) + "-" + timeRanges(1))
+            println("==================state: " + state + "==============pileState: " + pileJson.getString("pileState"))
+//            println("==================timeRanges: " + timeRanges(0) + "-" + timeRanges(1))
+            println("==================timeRanges: " + timeRange)
             println("==================chargeFee: " + chargeFee)
             println("==================serveFee: " + serveFee)
             println("==================ableNum: " + ableNum)
             println("==================price: " + price)
-            println("==================state: " + staOpstateInt + "==============staOpstate: " + staOpstate)
+            println("==================state: " + staOpstateInt + "==============staOpstate: " + staOpstateInt)
             println("==================dcableNum: " + dcableNum)
             println("==================acableNum: " + acableNum)
             println("==================" + result)
@@ -135,13 +110,27 @@ class MatchStreaming {
             //            println("==================四维：" + MongoDBUtil.search(query, MatchStreaming.table).first())
             //            println("=======================================================================================================")
           }
-
         }
       })
     })
-
   }
 
+  private def pileStateChange(pileState: String): Int = {
+    if (pileState == null || pileState.length() == 0) -1
+    else if (pileState.equals("空闲")) 0
+    else if (pileState.equals("已插枪") || pileState.equals("已充满") || pileState.equals("涓流充") || pileState.equals("充电中") || pileState.equals("暂停")) 1
+    else if (pileState.equals("离网") || pileState.equals("故障")) 2
+    else -1
+  }
+
+  private def staOpstateChange(staOpstate: String): Int = {
+    if (staOpstate == null || staOpstate.length() == 0) -1
+    else if (staOpstate.equals("初始") || staOpstate.equals("暂停营业")) 1
+    else if (staOpstate.equals("运营中")) 0
+    else if (staOpstate.equals("待运营")) 3
+    else if (staOpstate.equals("关闭")) 5
+    else -1
+  }
 }
 
 object MatchStreaming {
@@ -151,10 +140,8 @@ object MatchStreaming {
   val table = MongoDBUtil.getMongoDBTable("chargingPOI_match", "poi_dynamic")
 
   def main(args: Array[String]): Unit = {
-
     val ms = new MatchStreaming
-    ms.startStreaming("xdatanode-18:2181/kafka", "test", "TLD")
-
+    ms.startStreaming("192.168.4.196:2181/kafka", "test", "TLD")
   }
 
 }
